09c02e2bb2135233fc920085e56ffd705c8e6505
Anthonios Partheniou <partheniou@google.com>
2023-10-09 11:14:24 -0400

    Update DEFAULT_PYTHON_VERSION in noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -22,7 +22,7 @@ import nox
 BLACK_VERSION = "black==23.7.0"
 BLACK_PATHS = ["docs", "google", "tests", "noxfile.py", "setup.py"]
 
-DEFAULT_PYTHON_VERSION = "3.7"
+DEFAULT_PYTHON_VERSION = "3.8"
 CURRENT_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 
 
