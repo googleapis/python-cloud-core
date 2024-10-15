f8e464187d3d3b07eaeeb16335d715b0aaea402f
Rin Arakaki <rnarkkx@gmail.com>
2024-10-15 12:26:53 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,37 +20,11 @@ import setuptools
 
 # Package metadata.
 
-name = "google-cloud-core"
 description = "Google Cloud API client core library"
-# Should be one of:
-# 'Development Status :: 3 - Alpha'
-# 'Development Status :: 4 - Beta'
-# 'Development Status :: 5 - Production/Stable'
-release_status = "Development Status :: 5 - Production/Stable"
-dependencies = [
-    "google-api-core >= 1.31.6, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.0",
-    "google-auth >= 1.25.0, < 3.0dev",
-    "importlib-metadata > 1.0.0; python_version<'3.8'",
-]
-extras = {
-    "grpc": [
-        "grpcio >= 1.38.0, < 2.0dev",
-        "grpcio-status >= 1.38.0, < 2.0.dev0",
-    ],
-}
 
 # Setup boilerplate below this line.
 
 package_root = os.path.abspath(os.path.dirname(__file__))
-
-version = {}
-with open(os.path.join(package_root, "google/cloud/version.py")) as fp:
-    exec(fp.read(), version)
-version = version["__version__"]
-
-readme_filename = os.path.join(package_root, "README.rst")
-with io.open(readme_filename, encoding="utf-8") as readme_file:
-    readme = readme_file.read()
 
 # Only include packages under the 'google' namespace. Do not include tests,
 # benchmarks, etc.
@@ -61,34 +35,10 @@ packages = [
 ]
 
 setuptools.setup(
-    name=name,
-    version=version,
     description=description,
-    long_description=readme,
-    author="Google LLC",
-    author_email="googleapis-packages@google.com",
-    license="Apache 2.0",
     url="https://github.com/googleapis/python-cloud-core",
-    classifiers=[
-        release_status,
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Operating System :: OS Independent",
-        "Topic :: Internet",
-    ],
     platforms="Posix; MacOS X; Windows",
     packages=packages,
-    install_requires=dependencies,
-    extras_require=extras,
-    python_requires=">=3.7",
     include_package_data=True,
     zip_safe=False,
 )
