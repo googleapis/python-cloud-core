353b642cfe0846ab130abbba819eaa59eb954942
chalmer lowe <chalmerlowe@google.com>
2025-10-24 08:09:31 -0400

    fix(coverage): Remove obsolete Python < 3.8 import logic

--- google/cloud/obsolete/__init__.py
+++ google/cloud/obsolete/__init__.py
@@ -16,12 +16,7 @@
 
 import sys
 import warnings
-
-
-if sys.version_info < (3, 8):
-    import importlib_metadata as metadata
-else:
-    import importlib.metadata as metadata
+import importlib.metadata as metadata
 
 
 def complain(distribution_name):
